5cefe3fb384d8331fac5f13ebfbd3beb29ed1739
joaco036 <150397153+joaco036@users.noreply.github.com>
2024-08-15 19:13:37 -0300

    segundaPreEntrega1

---
+++ src/routes/views.router.js
@@ -0,0 +1,29 @@
+const { Router } = require("express")
+
+const router = Router()
+
+const users = [
+    {id:"1", first_name:"user exsample 1", Email:"user1@gmail.com"},
+    {id:"2", first_name:"user exsample 2", Email:"user2@gmail.com"},
+    {id:"3", first_name:"user exsample 3", Email:"user3@gmail.com"},
+]
+
+
+router.get("/", (req,res) => {
+
+    const userLogin={
+        first_name:" fede",
+        role: "admin"
+    }
+    res.render("index",{
+        user: userLogin,
+        isAdmin: userLogin.role === "admin",
+        users,
+        title:"home",
+        styles:"style.css",
+        isMenu: false,
+    })
+})
+
+
+module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express       = require("express")
 const userRouter    = require("./routes/users.router.js")
 const productRouter = require("./routes/porducts.router.js")
+const pruebaRouter = require("./routes/pruebas.router.js")
+const viewsRouter = require("./routes/views.router.js")
 const logger        = require("morgan")
 const { uploader }  = require("./utils/multer.js")
+const handlebars    = require("express-handlebars")
 
 // import express form express
 
@@ -17,16 +20,26 @@ app.use(express.urlencoded({extended:true}))
 app.use("/static", express.static(__dirname + "/public"))
 app.use (logger("dev"))
 
+//? configuracion del motro de plantillas
+
+app.engine("handlebars", handlebars.engine())
+// configurar carpeta donde tomar plantillas
+app.set("views", __dirname + "/views")
+// extencion de las plantillas
+app.set( "view engine","handlebars")
 
 app.use(function(req, res, next){
     console.log("time: ", Date.now())
     next()
 })
 
-//? endpoint
-app.post("/", uploader.single("myFile"), (req, res)=>{
+
+app.post("/updolader", uploader.single("myFile"), (req, res)=>{
     res.send("archivo subido")
 })
+
+app.use("/",viewsRouter)
+app.use("/prueba",pruebaRouter)
 app.use("/api/users",userRouter)
 app.use("/api/products", productRouter)
 // app.use("/api/carts", cartRouter)
